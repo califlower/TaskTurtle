3e3300356077a1c045176292475f66b0ea4663eb
olivergilan <olivergilan@gmail.com>
2020-04-08 23:38:10 -0400

    Created task file with stateful widget

---
+++ lib/task.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Task extends StatefulWidget{
+  Task(this.title, this.deadline);
+
+  final String title;
+  final DateTime deadline;
+  @override
+  _TaskState createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  String title;
+  DateTime deadline;
+  List subtasks;
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+
+}
