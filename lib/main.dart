bfa3d7ebb085c7f494f68fd9c7ca35ed2f0c68d5
Oliver Gilan <olivergilan@gmail.com>
2020-04-25 17:37:19 -0400

    created basic form to add new base tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'task.dart';
+import 'forms.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,17 +27,35 @@ class MyHomePage extends StatefulWidget {
   final List<Task> tasks;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState(tasks);
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState(this.tasks);
   Task focused;
+  bool adding;
+  List<Task> tasks;
 
   void setFocused(Task t){
     setState(() {
       focused = t;
+      adding = false;
     });
     print("clicked" + focused.title);
+  }
+
+  void showAdding(){
+    setState(() {
+      adding = true;
+    });
+  }
+
+  void addItem(Task t){
+    setState(() {
+      tasks.add(t);
+      adding = false;
+      focused = t;
+    });
   }
 
   @override
@@ -64,7 +83,23 @@ class _MyHomePageState extends State<MyHomePage> {
                       color: Color(0xff010f1c),
                       border: Border.all(color: Colors.red, width: 1),
                     ),
-                    child: Column(children: widget.tasks.map((e) => GestureDetector(onTap: () => setFocused(e),child:TaskShell(task:e))).toList()),
+                    child: Scaffold(
+                      body:Column(children: widget.tasks.map(
+                        (e) => Padding(
+                          padding: EdgeInsets.symmetric(vertical:10),
+                          child:GestureDetector(
+                            onTap: () => setFocused(e),
+                            child:TaskShell(task:e))
+                          )
+                        ).toList()
+                      ),
+                      backgroundColor: Color(0xff010f1c),
+                      floatingActionButton: FloatingActionButton(
+                        onPressed: showAdding,
+                        child: new Icon(Icons.add),
+                        backgroundColor: Color(0xff22eae0),
+                      ),
+                    )
                   ),
                 ),
                 Expanded(
@@ -76,8 +111,16 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     child: Column(
                       children: <Widget>[
-                        if(focused != null)TaskShell(task:focused),
-                        // Container(child: Column(children: focused.subtasks.map((s) => GestureDetector(onTap: ()=>setFocused(s), child:TaskShell(task:s))).toList()))
+                        if(adding == false)
+                          if(focused != null)...[
+                            TaskShell(task:focused),
+                            Container(child: Column(children: focused.subtasks.map((s) => GestureDetector(onTap: ()=>setFocused(s), child:TaskShell(task:s))).toList()))
+                          ],
+                        if(adding == true)
+                          Container(
+                            padding: EdgeInsets.all(25.0),
+                            child: AddForm(addItem),
+                          )
                       ],
                     )
                   ),
